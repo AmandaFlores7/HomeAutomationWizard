680863e722199191ab8093c934080d84f0496409
therexuz <ignavaldecace98@gmail.com>
2023-09-25 16:30:31 -0300

    Cambio estructura carpetas

--- HAW-FED/src/app/app-routing.module.ts
+++ HAW-FED/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { IntroduccionModule } from './introduccion/introduccion.module';
-import { PrimeraParteComponent } from './introduccion/primera-parte/primera-parte.component';
+import { IntroduccionModule } from './paginas/introduccion/introduccion.module';
+import { PrimeraParteComponent } from './paginas/introduccion/primera-parte/primera-parte.component';
 import { CarruselComponent } from './componentes/carrusel/carrusel.component';
 import { MenuInfoComponent } from './componentes/menu-info/menu-info.component';
 import { ImagenExplicativaComponent } from './componentes/imagen-explicativa/imagen-explicativa.component';
