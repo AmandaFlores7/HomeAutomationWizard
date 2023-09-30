c8db7859769238fe16eecdeb048b0bf97e7fa9c6
Amanda Flores <amanda.flores.a7@gmail.com>
2023-09-29 22:38:15 -0300

    menu info sin modales

--- HAW-FED/src/app/componentes/menu-info/menu-info.component.ts
+++ HAW-FED/src/app/componentes/menu-info/menu-info.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
+import { rutas } from 'src/app/constantes/rutas';
 
 @Component({
   selector: 'app-menu-info',
@@ -6,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./menu-info.component.scss']
 })
 export class MenuInfoComponent {
+  @Input() opciones : any;
+  informacion: any;
+  extra: any;
 
+  constructor(private router: Router) {
+    this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+    if (this.router?.url && this.buscarRuta(this.router.url)?.titulo != null) {
+      let infoPagina = this.buscarRuta(this.router.url);
+      this.informacion = infoPagina.informacion;
+      this.extra = infoPagina.extra.parrafo;
+    }
+    
+  }
+
+  buscarRuta(linkActual: string) {
+    let info = JSON.parse(JSON.stringify(rutas))[linkActual];
+    if (info) {
+      return info;
+    }
+    return null;
+  }
 }
--- HAW-FED/src/app/constantes/rutas.ts
+++ HAW-FED/src/app/constantes/rutas.ts
@@ -335,6 +335,31 @@ export const rutas ={
                 "titulo": "Siguiente",
                 "link": "/introduccion/3"
             }
+        },
+        "informacion": [
+            {
+                "titulo": "Conexión Inicial",
+                "parrafo": "Un dispositivo (llamado cliente) se conecta a un servidor MQTT."
+            },
+            {
+                "titulo": "Solicitud de Suscripción",
+                "parrafo": "El cliente le dice al servidor a qué temas o canales quiere suscribirse para recibir información."
+            },
+            {
+                "titulo": "Publicación de Mensajes",
+                "parrafo": "Otros dispositivos (llamados editores) envían mensajes al servidor, etiquetados con un tema específico."
+            },
+            {
+                "titulo": "Entrega de Mensajes",
+                "parrafo": "El servidor reenvía los mensajes a los clientes suscritos a esos temas."
+            },
+            {
+                "titulo": "Desconexión",
+                "parrafo": "Cuando un cliente ya no necesita comunicarse, se desconecta del servidor."
+            }
+        ],
+        "extra" : {
+            "parrafo": "Estos pasos permiten que los dispositivos se comuniquen y se mantengan actualizados sobre la información que les interesa."
         }
     },
     "/introduccion/3" : {
@@ -345,9 +370,15 @@ export const rutas ={
                 "link": "/introduccion/2"
             },
             "ruta2": {
-                "titulo": "Siguiente",
+                "titulo": "Comenzar",
                 "link": "/introduccion/4"
             }
+        },
+        "imagen": {
+            "ruta": "../../../../assets/img/led2.jpeg"
+        },
+        "extra": {
+            "parrafo": "En esta animación, se puede visualizar el funcionamiento básico del protocolo. Fuente: Amazon Web Services."
         }
     },
     "/introduccion/4" : {
