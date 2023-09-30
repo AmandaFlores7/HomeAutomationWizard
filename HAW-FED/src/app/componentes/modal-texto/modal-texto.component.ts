5695c62fdebd62a79f9fb5e605f54884750bbd7e
Amanda Flores <amanda.flores.a7@gmail.com>
2023-09-30 04:22:12 -0300

    mejora modal - mas flexibilidad

--- HAW-FED/src/app/componentes/modal-texto/modal-texto.component.ts
+++ HAW-FED/src/app/componentes/modal-texto/modal-texto.component.ts
@@ -1,15 +1,36 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-modal-texto',
   templateUrl: './modal-texto.component.html',
   styleUrls: ['./modal-texto.component.scss']
 })
-export class ModalTextoComponent {
+export class ModalTextoComponent implements OnInit {
   @Input() data: any;
+  @Input() displayStyle: string = '';
   @Output() closeModal = new EventEmitter();
+
+  mostrarFooter = false;
+
+  constructor() {
+  }
 
   onCloseModal(): void {
     this.closeModal.emit();
+  }
+
+  ngOnInit() {
+    var elemento = document.getElementById('contenidoModal');
+    if (elemento) {
+      if (this.displayStyle === 'success') {
+        elemento.classList.add('text-white');
+        elemento.classList.add('bg-success');
+        this.mostrarFooter = true;
+      } else if (this.displayStyle === 'error') {
+        elemento.classList.add('text-white');
+        elemento.classList.add('bg-danger');
+        this.mostrarFooter = true; // TODO: cambiar a false
+      }
+    }
   }
 }
