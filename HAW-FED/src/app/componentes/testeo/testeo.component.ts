e6a87387ed7563120cebd08befc8bdb7d415926c
Amanda Flores <amanda.flores.a7@gmail.com>
2023-09-30 04:24:59 -0300

    uso modal componente en testeo

--- HAW-FED/src/app/componentes/testeo/testeo.component.ts
+++ HAW-FED/src/app/componentes/testeo/testeo.component.ts
@@ -1,48 +1,54 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { MqttserviceService } from 'src/app/servicios/mqttservice.service';
-declare let window:any;
+declare let window: any;
 
 @Component({
   selector: 'app-testeo',
   templateUrl: './testeo.component.html',
-  styleUrls: ['./testeo.component.scss']
+  styleUrls: ['./testeo.component.scss'],
 })
 export class TesteoComponent {
-  messages: string[] = ['Testeando conexión...', 'Creando suscripción al tópico: /test-result...', 'Publicando mensaje de testeo al tópico: /test-mqtt...', 'Verificando recepción de mensaje...', 'Finalizando...']
+  messages: string[] = [
+    'Testeando conexión...',
+    'Creando suscripción al tópico: /test-result...',
+    'Publicando mensaje de testeo al tópico: /test-mqtt...',
+    'Verificando recepción de mensaje...',
+    'Finalizando...',
+  ];
   currentMessageIndex: number = 0;
   progressValue = 0;
   test_result: any;
   isLoading: boolean = true;
-  displaySuccessMessage: boolean = false;
-  displayErrorMessage: boolean = false;
-  displayStyle = "none";
-  errorModal:any;
-  successModal:any;
+  mostrarBoton: boolean = false;
 
-  constructor(private _mqttService: MqttserviceService) { }
+  displayStyle = 'none';
+  hayMensaje: boolean = false;
+  attrMensaje: any;
+  route: any;
+
+  constructor(private _mqttService: MqttserviceService, route: Router) {}
 
   ngOnInit() {
+    this.initLoading();
+  }
 
-    this.errorModal = new window.bootstrap.Modal(
-      document.getElementById('errorMessageModal')
-    )
+  initLoading() {
+    this.currentMessageIndex = 0;
+    this.progressValue = 0;
+    this.isLoading = true;
+    this.test_result = null;
 
-    this.successModal = new window.bootstrap.Modal(
-      document.getElementById('successMessageModal')
-    )
-
-    this._mqttService.testMqttProtocol().subscribe(async res => {
+    this._mqttService.testMqttProtocol().subscribe(async (res) => {
       setTimeout(() => {
-        this.test_result = res
+        this.test_result = res;
       }, 1500);
     });
     this.startLoading(this.test_result);
   }
 
   startLoading(test_result: any) {
-
     const loadingTime = 5000; // 5 segundos
-    
 
     const interval = setInterval(() => {
       this.progressValue += 1200 / (loadingTime / 100);
@@ -51,19 +57,33 @@ export class TesteoComponent {
         clearInterval(interval);
 
         if (this.test_result) {
-          if (this.test_result['test-result'] === "ok") {
-            this.displaySuccessMessage = true;
-            this.openSuccessModal();
+          if (this.test_result['test-result'] === 'ok') {
+            this.displayStyle = 'success';
+            this.hayMensaje = true;
+            this.attrMensaje = {
+              titulo: 'Éxito',
+              parrafo:
+                'La conexión con el broker MQTT se ha establecido correctamente.',
+            };
           } else {
-            this.displayErrorMessage = true;
-            this.openErrorModal()
+            this.displayStyle = 'error';
+            this.hayMensaje = true;
+            this.attrMensaje = {
+              titulo: 'Error',
+              parrafo:
+                'Hay error de conexión. :(',
+            };
           }
         } else {
-          this.displayErrorMessage = true;
-          this.openErrorModal()
+          this.hayMensaje = true;
+          this.displayStyle = 'error';
+          this.attrMensaje = {
+            titulo: 'Error',
+              parrafo:
+                'Hay error de conexión. :(',
+          };
         }
       } else {
-
         // Incrementa el índice del mensaje actual
         this.currentMessageIndex++;
         // Verifica si hemos llegado al final de los mensajes y reinicia si es necesario
@@ -73,13 +93,11 @@ export class TesteoComponent {
       }
     }, 2000);
   }
-
-  openErrorModal() {
-    this.errorModal.show();
+  setSelectedMessage(msg: any, displayStyle: string) {
+    this.hayMensaje = msg;
+    this.progressValue = 0;
+    if (displayStyle === 'error') {
+      this.mostrarBoton = true;
+    }
   }
-
-  openSuccessModal(){
-    this.successModal.show();
-  }
- 
 }
