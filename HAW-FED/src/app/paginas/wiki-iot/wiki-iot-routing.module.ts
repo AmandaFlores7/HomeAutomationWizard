359e997940a75912dbff0cfbf76e0a3f0e555695
Amanda Flores <amanda.flores.a7@gmail.com>
2023-11-09 17:26:03 -0300

    .

--- HAW-FED/src/app/paginas/wiki-iot/wiki-iot-routing.module.ts
+++ HAW-FED/src/app/paginas/wiki-iot/wiki-iot-routing.module.ts
@@ -20,19 +20,6 @@ const routes: Routes = [
   { path: 'leccion4/5', component: ContenidoWikiComponent},
   { path: 'leccion5', component: LeccionComponent},
   { path: 'leccion6', component: LeccionComponent},
-  // { path: 'leccion1/contenido/1', component:ContenidoWikiComponent},
-  // { path: 'leccion1/contenido/2', component:ContenidoWikiComponent},
-  // { path: 'leccion2/contenido/1', component: ContenidoWikiComponent },
-  // { path: 'leccion2/contenido/2', component: ContenidoWikiComponent },
-  // { path: 'leccion3/contenido/1', component: ContenidoWikiComponent },
-  // { path: 'leccion3/contenido/2', component: ContenidoWikiComponent },
-  // { path: 'leccion3/contenido/3', component: ContenidoWikiComponent },
-  // { path: 'leccion3/contenido/4', component: ContenidoWikiComponent },
-  // { path: 'leccion3/contenido/5', component: ContenidoWikiComponent },
-  // { path: 'leccion4/contenido/1', component: ObjetivosAprendizajeComponent },
-  // { path: 'leccion5/contenido/1', component: ObjetivosAprendizajeComponent },
-  // { path: 'leccion6/contenido/1', component: ObjetivosAprendizajeComponent },
-  // { path: 'leccion7/contenido/1', component: ObjetivosAprendizajeComponent }
 ];
 
 @NgModule({
