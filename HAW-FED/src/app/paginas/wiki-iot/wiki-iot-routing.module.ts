af086d03a89e12b9dc98d9a7e29d1e8426733019
Amanda Flores <amanda.flores.a7@gmail.com>
2023-11-09 17:51:24 -0300

    menus mas menusss

--- HAW-FED/src/app/paginas/wiki-iot/wiki-iot-routing.module.ts
+++ HAW-FED/src/app/paginas/wiki-iot/wiki-iot-routing.module.ts
@@ -3,16 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { ObjetivosAprendizajeComponent } from './objetivos-aprendizaje/objetivos-aprendizaje.component';
 import { ContenidoWikiComponent } from './contenido-wiki/contenido-wiki.component';
 import { LeccionComponent } from './leccion/leccion.component';
+import { MenuOpcionesComponent } from 'src/app/componentes/menu-opciones/menu-opciones.component';
 
 const routes: Routes = [
   { path: 'leccion1', component: ObjetivosAprendizajeComponent },
   { path: 'leccion2', component: LeccionComponent},
   { path: 'leccion2/1', component: ContenidoWikiComponent},
   { path: 'leccion2/2', component: ContenidoWikiComponent},
-  { path: 'leccion3', component: LeccionComponent},
+  { path: 'leccion3', component: MenuOpcionesComponent},
   { path: 'leccion3/1', component: ContenidoWikiComponent},
   { path: 'leccion3/2', component: ContenidoWikiComponent},
-  { path: 'leccion4', component: LeccionComponent},
+  { path: 'leccion4', component: MenuOpcionesComponent},
   { path: 'leccion4/1', component: ContenidoWikiComponent},
   { path: 'leccion4/2', component: ContenidoWikiComponent},
   { path: 'leccion4/3', component: ContenidoWikiComponent},
