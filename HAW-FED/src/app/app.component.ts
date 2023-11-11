a0717c7e1d98ea6b7f59621b92b2c71cfc5321b6
Amanda Flores <amanda.flores.a7@gmail.com>
2023-11-11 11:28:33 -0300

    new ip mandy 88->83

--- HAW-FED/src/app/app.component.ts
+++ HAW-FED/src/app/app.component.ts
@@ -63,18 +63,18 @@ export class AppComponent {
 const ipModificables = {
   ip_local: '192.168.2.1',
   ip_local2: '127.0.0.1',
-  ip_local3: '192.168.1.88',
+  ip_local3: '192.168.1.83',
   ip_local4: '192.168.0.105',
   api_url: 'http://192.168.2.1:8000',
   api_url2: 'http://127.0.0.1:8000',
-  api_url3: 'http://192.168.1.88:8000',
+  api_url3: 'http://192.168.1.83:8000',
   ws_url: 'ws://192.168.2.1:8000',
   ws_url2: 'ws://127.0.0.1:8000',
-  ws_url3: 'ws://192.168.1.88:8000',
+  ws_url3: 'ws://192.168.1.83:8000',
   ws_url4: 'ws://192.168.0.105:8000',
   url_camera: 'http://192.168.2.1:8081',
   url_camera2: 'http://127.0.0.1:8081',
-  url_camera3: 'http://192.168.1.88:8081'
+  url_camera3: 'http://192.168.1.83:8081'
 };
 export const CONSTANTES = {
   ip_local: ipModificables.ip_local,
